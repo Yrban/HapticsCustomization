9ef7f602f6d0e066108692dd225a6ef1673cda33
Bryan <bryan@cimcal.com>
2022-09-14 11:39:34 -0400

    Initial Commit

---
+++ HapticsCustomization/HapticsCustomizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsCustomizationApp.swift
+//  HapticsCustomization
+//
+//  Created by Bryan on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct HapticsCustomizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
